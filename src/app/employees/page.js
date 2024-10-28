11294b27c0cfa62c4c00347da38f3da0de3e3698
TheHamer <66170230+TheHamer@users.noreply.github.com>
2024-10-28 16:33:28 +0000

    Table ajusted and button for new employee added

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -2,6 +2,15 @@ import Table from "../../components/Table"
 
 export default function TablePage() {
     return (
-        <Table></Table>
+        <div className="w-full">
+            <div className="pt-10 pb-10 ps-5 pe-5">
+                <a href="#" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Add Employee
+                </a>
+            </div>
+            <div className="ps-5 pe-5">
+                <Table></Table>
+            </div>
+        </div>
     )
 }

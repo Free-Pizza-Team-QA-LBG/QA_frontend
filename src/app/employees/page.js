c1ed36a1578a27c901c4769589de910efb8e1815
TheHamer <66170230+TheHamer@users.noreply.github.com>
2024-10-29 15:46:05 +0000

    Added edit and remove modals

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -8,10 +8,22 @@ import Modal from "@/components/Modal"
 
 export default function TablePage() {
     const [showModal, setShowModal] = useState(false);
+    const [showModalUpdate, setShowModalUpdate] = useState({id: 0, show: false});
+    const [showModalDelete, setShowModalDelete] = useState({id: 0, show: false});
+
 
     const toggleModal = () => {
       setShowModal(!showModal);
     };
+
+    const toggleModalUpdate = (id) => {
+        setShowModalUpdate({id: id, show: !showModalUpdate.show});
+      };
+
+    const toggleModalDelete = (id) => {
+        setShowModalDelete({id: id, show: !showModalDelete.show});
+      };
+
 
     return (
         <div className="w-full pb-10">
@@ -23,13 +35,19 @@ export default function TablePage() {
             <Modal show={showModal} onClose={toggleModal} title="Add Employee">
                 This is where the form will go
             </Modal>
+            <Modal show={showModalUpdate.show} onClose={toggleModalUpdate} title="Update Emplyee">
+            This is where the update form will go for employee {showModalUpdate.id}
+            </Modal>
+            <Modal show={showModalDelete.show} onClose={toggleModalDelete} title="Delete Emplyee">
+                Are you sure you want to delete employee {showModalDelete.id}?
+            </Modal>
             {/*
             <div className="text-2xl text-gray-900 pt-0 pb-3 ps-5 pe-5">
                 <h1 className="text-gray-900">All Employees</h1>
             </div>
             */}
             <div className="ps-5 pe-5">
-                <Table></Table>
+                <Table onModalUpdate={toggleModalUpdate} onModalDelete={toggleModalDelete}></Table>
             </div>
         </div>
     )
--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,4 +1,16 @@
-export default function Tr({id, firstName, lastName, email, department, salary}) {
+import { useState } from 'react';
+import Modal from "@/components/Modal"
+
+export default function Tr({id, firstName, lastName, email, department, salary, toggleModelUpdate, toggleModelDelete}) {
+
+    const createModelUpdate = () => {
+        toggleModelUpdate(id)
+    }
+
+    const createModelDelete = () => {
+        toggleModelDelete(id)
+    }
+    
     return (
         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
             <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -17,10 +29,10 @@ export default function Tr({id, firstName, lastName, email, department, salary})
                 {salary || "-"}
             </td>
             <td className="px-6 py-4">
-                <a href="#" className="bg-yellow-400 text-white py-2 px-4 text-sm rounded hover:bg-yellow-300">Edit</a>
+                <a href="#" className="bg-yellow-400 text-white py-2 px-4 text-sm rounded hover:bg-yellow-300" onClick={createModelUpdate}>Edit</a>
             </td>
             <td className="flex items-center px-6 py-4">
-                <a href="#" className="bg-red-400 text-white py-2 px-4 text-sm rounded hover:bg-red-300">Remove</a>
+                <a href="#" className="bg-red-400 text-white py-2 px-4 text-sm rounded hover:bg-red-300" onClick={createModelDelete}>Remove</a>
             </td>
         </tr>
     )

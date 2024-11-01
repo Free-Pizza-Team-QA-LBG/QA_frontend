50ca386f3bf8c66a48192d12c154db750e09f4c4
Troy Cooper <troy@troyc.dev>
2024-11-01 12:27:48 +0000

    Make request to add employee

--- src/app/employees/page.js
+++ src/app/employees/page.js
@@ -11,13 +11,13 @@ import DeleteEmployeeForm from "@/components/DeleteEmployeeForm"
 
 
 export default function TablePage() {
-    const [showModal, setShowModal] = useState(false);
+    const [showModalAdd, setShowModalAdd] = useState(false);
     const [showModalUpdate, setShowModalUpdate] = useState({data: {}, show: false});
     const [showModalDelete, setShowModalDelete] = useState({id: 0, show: false});
 
 
-    const toggleModal = () => {
-      setShowModal(!showModal);
+    const toggleModalAdd = () => {
+      setShowModalAdd(!showModalAdd);
     };
 
     const toggleModalUpdate = (da) => {
@@ -32,6 +32,53 @@ export default function TablePage() {
     const [emplLoading, setEmplLoading] = useState(true);
     const [emplData, setEmplData] = useState(null);
     const [emplError, setEmplError] = useState(null);
+
+    const addEmployee = e => {
+        e.preventDefault();
+        
+        const firstName = e.target[0].value;
+        const lastName = e.target[1].value;
+        const email = e.target[2].value;
+        const department = e.target[3].value;
+        const salary = e.target[4].value;
+
+        fetch("http://localhost:8080/api/v1/employee", {
+            method: "POST",
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                email,
+                department,
+                salary,
+            }),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                console.log(json);
+
+                setEmplData([...emplData, json.data])
+
+                toggleModalAdd()
+            })
+            .catch((err) => {
+                setEmplError(err.message ?? "Error");
+                setEmplLoading(false);
+            });
+    };
+
+    async function deleteEmployee(id) {
+        console.log(`http://localhost:8080/api/v1/employee/${id}`)
+        const response = await fetch(`http://localhost:8080/api/v1/employee/${id}`, {
+            method: 'DELETE',
+                });
+                setEmplData(emplData.filter(employee => employee.id !== id))
+                toggleModalDelete(-1)
+            }
+    
     useEffect(() => {
         fetch("http://localhost:8080/api/v1/employee/all")
             .then((res) => res.json())
@@ -45,24 +92,15 @@ export default function TablePage() {
             });
     }, []);
 
-    async function deleteEmployee(id) {
-        console.log(`http://localhost:8080/api/v1/employee/${id}`)
-        const response = await fetch(`http://localhost:8080/api/v1/employee/${id}`, {
-            method: 'DELETE',
-                });
-                setEmplData(emplData.filter(employee => employee.id !== id))
-                toggleModalDelete(-1)
-            }
-
     return (
         <div className="w-full pb-10">
             <div className="pt-10 pb-10 ps-5 pe-5">
-                <a href="#" className="bg-yellow-400 text-black py-4 px-8 text-lg font-semibold uppercase rounded-full transition-colors duration-300 hover:bg-yellow-300" onClick={toggleModal}>
+                <a href="#" className="bg-yellow-400 text-black py-4 px-8 text-lg font-semibold uppercase rounded-full transition-colors duration-300 hover:bg-yellow-300" onClick={toggleModalAdd}>
                     Add Employee
                 </a>
             </div>
-            <Modal show={showModal} onClose={toggleModal} title="Add Employee">
-                <AddEmployeeForm />
+            <Modal show={showModalAdd} onClose={toggleModalAdd} title="Add Employee">
+                <AddEmployeeForm onAddSubmit={addEmployee} />
             </Modal>
             <Modal show={showModalUpdate.show} onClose={toggleModalUpdate} title="Update Emplyee">
                 <UpdateEmployeeForm data={showModalUpdate.data}/>
--- src/components/AddEmployeeForm.jsx
+++ src/components/AddEmployeeForm.jsx
@@ -1,23 +1,20 @@
-import { useForm } from 'react-hook-form';
-
-export default function AddEmployeeForm() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-
-  const onSubmit = data => {
-    console.log(data);
-    // Handle form submission here
-  };
-
+export default function AddEmployeeForm({ onAddSubmit }) {
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="bg-white p-5 rounded-lg shadow-lg m-0 text-black">
+    <form onSubmit={onAddSubmit} className="bg-white p-5 rounded-lg shadow-lg m-0 text-black">
       <div className="mb-0">
-        <label htmlFor="name" className="block text-gray-700">Name</label>
+        <label htmlFor="name" className="block text-gray-700">First Name</label>
         <input 
-          id="name"
-          {...register('name', { required: true })}
+          id="firstName"
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
         />
-        {errors.name && <p className="text-red-500 mt-1">This field is required</p>}
+      </div>
+
+      <div className="mb-0">
+        <label htmlFor="name" className="block text-gray-700">Last Name</label>
+        <input 
+          id="lastName"
+          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+        />
       </div>
 
       <div className="mb-4">
@@ -25,20 +22,24 @@ export default function AddEmployeeForm() {
         <input 
           id="email"
           type="email"
-          {...register('email', { required: true })}
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
         />
-        {errors.email && <p className="text-red-500 mt-1">This field is required</p>}
       </div>
 
       <div className="mb-4">
-        <label htmlFor="position" className="block text-gray-700">Position</label>
+        <label htmlFor="department" className="block text-gray-700">Department</label>
         <input 
-          id="position"
-          {...register('position', { required: true })}
+          id="department"
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
         />
-        {errors.position && <p className="text-red-500 mt-1">This field is required</p>}
+      </div>
+
+      <div className="mb-4">
+        <label htmlFor="salary" className="block text-gray-700">Salary</label>
+        <input 
+          id="salary"
+          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm"
+        />
       </div>
 
       <div className="flex justify-end">

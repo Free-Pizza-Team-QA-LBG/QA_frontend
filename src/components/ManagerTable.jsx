de940b5c98a04a8c79e3d75a80b978c8610ffa7a
Alex Terry <28978970+AlexPTerry@users.noreply.github.com>
2024-11-08 11:40:21 +0000

    Added manager page

---
+++ src/components/ManagerTable.jsx
@@ -0,0 +1,29 @@
+import ManagerTableRow from "./ManagerTableRow";
+
+function ManagerTable({ data }) {
+
+    return (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" className="px-6 py-3">
+                            Manager ID
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                            Employee ID
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {
+                            data.map((obj, key) => <ManagerTableRow {...obj} key={key}/>)
+                        }
+                </tbody>
+            </table>
+        </div>
+
+    );
+    }
+    
+export default Table;

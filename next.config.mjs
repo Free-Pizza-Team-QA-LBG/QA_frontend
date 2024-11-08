e51b57261bdf4867117c368e38f4a0fb853661d7
Alex Terry <28978970+AlexPTerry@users.noreply.github.com>
2024-11-08 10:20:09 +0000

    Added backend IP  env var to nex.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_BACKEND_IP: process.env.NEXT_PUBLIC_BACKEND_IP,
+  },
 };
 
 export default nextConfig;
